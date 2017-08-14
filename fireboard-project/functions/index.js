d66e80474c66cbab9897b5dfa4bde70a1919857d
rklmallari <raykevinmallari@yahoo.com>
2017-08-14 15:45:02 +0800

    Added function for adding course and updating profile

--- fireboard-project/functions/index.js
+++ fireboard-project/functions/index.js
@@ -7,6 +7,14 @@ admin.initializeApp({
   databaseURL: "https://fireboard-hackathon2017.firebaseio.com"
 });
 
+const nodemailer = require('nodemailer');
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(
+    `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+const APP_NAME = 'Fireboards';
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -36,3 +44,24 @@ exports.getUIDByEmail = functions.https.onRequest((request, response) => {
   });
   
 });
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendWelcomeEmail(email, displayName);
+});
+
+function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. Thank you for registering. As a new learner, please browse through our available training materials on the site.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('A welcome email was sent to:', email);
+  });
+}
